b5e967492d4d3912e66d9cc20c8da0c9bbfe1f5a
DevBDarius <d.burinskis@gmail.com>
2021-11-27 23:10:52 +0000

    JS created. Email service complete

---
+++ script.js
@@ -0,0 +1,27 @@
+// EmailJS info:
+// template_zjc3dh6
+// service_6s7dn1m
+// user_D6eyHn0LjmGYwhBEntOlB
+
+function contact(event) {
+  event.preventDefault();
+  const loading = document.querySelector('.modal__overlay--loading');
+  const success = document.querySelector('.modal__overlay--success');
+  loading.classList += " modal__overlay--visible"
+  
+  emailjs.sendForm(
+    'service_6s7dn1m',
+    'template_zjc3dh6',
+    event.target,
+    'user_D6eyHn0LjmGYwhBEntOlB'
+  ).then(() => {
+    loading.classList.remove("modal__overlay--visible");
+    success.classList += " modal__overlay--visible";
+  }).catch(() => {
+    loading.classList.remove("modal__overlay--visible");
+    alert(
+      "The email service is temporarily unavailable. Please email me directly at d.burinskis@gmail.com"
+    )
+  })
+
+}
