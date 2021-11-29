86c597befbd879542983ddcb7a55c19a96fdef8e
DevBDarius <d.burinskis@gmail.com>
2021-11-29 12:21:32 +0000

    Cool squiggly shapes background

--- script.js
+++ script.js
@@ -4,6 +4,21 @@
 // user_D6eyHn0LjmGYwhBEntOlB
 let isModalOpen = false;
 let contrastModeOn = false;
+const scaleFactor = 1 / 20;
+
+function moveBackground(event) {
+  const shapes = document.querySelectorAll(".shape");
+  const x = event.clientX * scaleFactor;
+  const y = event.clientY * scaleFactor;
+
+  for (let i = 0; i < shapes.length; i++) {
+    const isOdd = i%2 !== 0;
+    const boolInt = isOdd ? -1 : 1;
+    shapes[i].style.transform =
+      `translate(${x * boolInt}px, ${y * boolInt}px)`;
+
+  }
+}
 
 function toggleContrast() {
   contrastModeOn = !contrastModeOn;
